3a7a4b3bad97e0ff03979fa58988642a63c2e151
Carol Booth <carol.booth2@newcastle.ac.uk>
2025-04-03 12:55:00 +0100

    Added additional analysis functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,6 +4,7 @@ import json
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re # added this line
 
 def main(input_file, output_file, graph_file):
     print("--START--")
@@ -17,7 +18,37 @@ def main(input_file, output_file, graph_file):
 # https://data.nasa.gov/resource/eva.json (with modifications)
 
 
-## read in the data and clean it up ##
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
 
 def read_json_to_dataframe(input_file):
     """ read teh data from a JSON file into pandas dataframe
