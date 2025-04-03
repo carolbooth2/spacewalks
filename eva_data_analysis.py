6691c6b26f84c412d0ccee2ed03c651482c75c16
Carol Booth <carol.booth2@newcastle.ac.uk>
2025-04-03 10:09:25 +0100

    added explanatory comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -10,6 +10,7 @@ input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv','w')
 graph_file = 'myplot.png'
 
+## read in the data and clean it up ##
 # read in json file
 eva_df = pd.read_json(input_file, convert_dates=['date'])
 # convert eva column to float
@@ -19,15 +20,18 @@ eva_df.dropna(axis=0, inplace=True)
 # sort values by date
 eva_df.sort_values('date', inplace=True)
 
+
 # write out data to csv
 
 eva_df.to_csv(output_file, index=False)
 
-
+## create columns to plot
 #create new column duration hours
 eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-
 eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
+
+# plot date against cumulative time, save to file and show on screen
+
 plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
