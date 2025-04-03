1c3aa40678815c74d6e3469ac5785d279cddb090
Carol Booth <carol.booth2@newcastle.ac.uk>
2025-04-03 10:05:30 +0100

    changed code to use pandas functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -2,12 +2,41 @@ import datetime as dt
 import csv
 import json
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 # https://data.nasa.gov/resource/eva.json (with modifications)
 input_file = open('./eva-data.json', 'r')
 output_file = open('./eva-data.csv','w')
 graph_file = 'myplot.png'
 
+# read in json file
+eva_df = pd.read_json(input_file, convert_dates=['date'])
+# convert eva column to float
+eva_df['eva'] = eva_df['eva'].astype(float)
+# removing missing values
+eva_df.dropna(axis=0, inplace=True)
+# sort values by date
+eva_df.sort_values('date', inplace=True)
+
+# write out data to csv
+
+eva_df.to_csv(output_file, index=False)
+
+
+#create new column duration hours
+eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+
+eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
+plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
+plt.xlabel('Year')
+plt.ylabel('Total time spent in space to date (hours)')
+plt.tight_layout()
+plt.savefig(graph_file)
+plt.show()
+
+
+'''
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
 data=[]
@@ -64,3 +93,4 @@ plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
 plt.show()
+'''
